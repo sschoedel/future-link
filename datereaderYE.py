d5d02ff1071968d6a593dcabec957f2b18a70335
Conner <cwilliampulling@vt.edu>
2020-05-31 01:49:44 -0400

    lstm  data with x, y, and state order as well as updated final dataset.py

--- datereaderYE.py
+++ datereaderYE.py
@@ -4,6 +4,10 @@ from datetime import datetime, timedelta
 import numpy as np
 import dataLoad
 import pandas as pd
+import pdb
+
+from itertools import chain
+import joblib as jb
 
 def get_X(start_date, end_date, state_name):
     start_date = datetime.strptime(start_date.strip('\"').strip('\n'), '%Y-%m-%d')
@@ -37,15 +41,42 @@ def get_X(start_date, end_date, state_name):
         curr_day += timedelta(days = 1)
     colLab = [feat[:len(feat) - 11] for i, feat in enumerate(features) if i % 2 == 0]
         
-    return features, pd.DataFrame(data = np.array(xPol), columns = colLab)
+    return pd.DataFrame(data = np.array(xPol), columns = colLab)
+
+def get_mulList(*args):
+    return map(list,zip(*args))
+
+def get_Ydict():   
+    csv_data = open('NO2_data.csv','r')
+    data = list(csv.reader(csv_data))
+    ind_dict = dict(zip([key.lower() for key in data[0]],[[abs(float(value)) for value in values] for values in get_mulList(*data[1:])]))
+
+    return ind_dict, list(ind_dict)
+
+def get_XY(end_d, start_d = '2020-02-22'):
+    y_dict, states = get_Ydict()
+    start_date = datetime.strptime(start_d, '%Y-%m-%d')
+    end_date = datetime.strptime(end_d, '%Y-%m-%d')
+    num_days = (end_date - start_date).days
+    X = []
+    Y = []
+    stateOrder = []
+    for state in states:
+        print(state)
+        y = y_dict[state][:num_days]
+        x = get_X(start_d, end_d, state)
+        #pdb.set_trace()
+        X.append(x)
+        Y.append(y)
+        stateOrder.append(state)
+
+    return X, Y, stateOrder
+
 
 if __name__ == '__main__':
-    labels, output = get_X("2020-02-22", "2020-05-28", 'virginia')
-    print(labels)
-    print(output)
-
-
-
-
-
-
+    x, y, states = get_XY('2020-05-28')
+    print(len(x))
+    dumpObj = [x,y,states]
+    print(dumpObj)
+    jb.dump(dumpObj, "lstm_Data.joblib")
+    #jb.dump(y, "y_NO2.joblib")
